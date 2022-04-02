420caee98f12caf427f390a43c556e763746a223
mochysn <moch.ysna@gmail.com>
2022-04-02 19:44:02 +0700

    Initial commit

---
+++ app/src/main/java/com/moch/pert12_otp/Api.kt
@@ -0,0 +1,16 @@
+package com.moch.pert12_otp
+
+import io.reactivex.Observable
+import okhttp3.ResponseBody
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface Api {
+
+    @POST("otp/send")
+    fun sendOtp(@Query("phoneNumber", encoded = true) phoneNumber: String): Observable<ResponseBody>
+
+    @POST("otp/update")
+    fun updateOtp(@Query("codeOtp") codeOtp: String): Observable<ResponseBody>
+
+}
---
+++ app/src/main/java/com/moch/pert12_otp/SmsListener.kt
@@ -0,0 +1,7 @@
+package com.moch.pert12_otp
+
+interface SmsListener {
+
+    fun messageReceived(message: String)
+
+}
